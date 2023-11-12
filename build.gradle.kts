ffb0ceacac8db5a127734fd3e24cbf6549187105
RappyTV <rappytv@rappytv.com>
2023-11-12 20:29:25 +0100

    Register 1.20.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,8 @@ labyMod {
                 "1.19.2",
                 "1.19.3",
                 "1.19.4",
-                "1.20.1"
+                "1.20.1",
+                "1.20.2"
         ) { version, provider ->
             configureRun(provider, version)
         }
