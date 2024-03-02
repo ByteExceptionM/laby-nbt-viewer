32ce657df8d49c949b9fcc4e1dea0823c4574f2d
RappyTV <rappytv@rappytv.com>
2024-03-03 00:37:45 +0100

    Use newest labygradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "laby-nbt-addon"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.3.30"
+    val labyGradlePluginVersion = "0.3.44"
     plugins {
         id("net.labymod.gradle") version (labyGradlePluginVersion)
     }
