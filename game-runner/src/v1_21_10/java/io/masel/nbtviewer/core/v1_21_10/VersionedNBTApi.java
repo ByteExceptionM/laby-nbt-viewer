1353a316647499b6484a39b5a0eba6ca2c2a184d
RappyTV <rappytv@rappytv.com>
2025-11-03 22:43:19 +0100

    feat: Add 1.21.10 implementation

---
+++ game-runner/src/v1_21_10/java/io/masel/nbtviewer/core/v1_21_10/VersionedNBTApi.java
@@ -0,0 +1,78 @@
+package io.masel.nbtviewer.core.v1_21_10;
+
+import com.google.gson.*;
+import io.masel.nbtviewer.api.NBTApi;
+import net.labymod.api.component.data.DataComponentContainer;
+import net.labymod.api.component.data.DataComponentKey;
+import net.labymod.api.models.Implements;
+import net.minecraft.client.Minecraft;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.NumericTag;
+import net.minecraft.nbt.StringTag;
+import org.jetbrains.annotations.NotNull;
+
+import javax.inject.Singleton;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+@Singleton
+@Implements(NBTApi.class)
+public class VersionedNBTApi extends NBTApi {
+
+    @Override
+    public boolean hasAdvancedToolsTips() {
+        return Minecraft.getInstance().options.advancedItemTooltips;
+    }
+
+    @Override
+    public String prettyPrint(DataComponentContainer components) {
+        JsonObject jsonObject = new JsonObject();
+
+        List<DataComponentKey> dataComponentKeys = new ArrayList<>(components.keySet());
+        dataComponentKeys.sort(Comparator.comparing(DataComponentKey::name));
+
+        for (DataComponentKey dataComponentKey : dataComponentKeys) {
+            Object value = components.get(dataComponentKey);
+
+            if (value == null || this.parseValue(value).isJsonNull())
+                continue;
+
+            jsonObject.add(dataComponentKey.name(), this.parseValue(value));
+        }
+
+        return this.gson.toJson(jsonObject);
+    }
+
+    private JsonElement parseValue(@NotNull Object content) {
+        try {
+            return switch (content) {
+                case NumericTag value -> new JsonPrimitive(value.toString());
+                case StringTag value -> {
+                    try {
+                        yield JsonParser.parseString(value.toString());
+                    } catch (Throwable ignored) {
+                        yield new JsonPrimitive(value.toString());
+                    }
+                }
+                case CompoundTag value -> this.gson.fromJson(value.toString(), JsonObject.class);
+                case ListTag value -> {
+                    JsonArray jsonArray = new JsonArray();
+
+                    for (Object element : value.toArray()) {
+                        jsonArray.add(this.parseValue(element));
+                    }
+
+                    yield jsonArray;
+                }
+                default -> new JsonPrimitive(content.toString());
+            };
+        } catch (Throwable cause) {
+            cause.printStackTrace();
+        }
+
+        return JsonNull.INSTANCE;
+    }
+
+}
