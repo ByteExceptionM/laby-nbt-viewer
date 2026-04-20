bfd2935c9d36140399663a5ecd4283830f0f6635
masel.io <git@byteexception.eu>
2026-04-20 11:56:42 +0200

    chore: Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 val versions = providers.gradleProperty("net.labymod.minecraft-versions").get().split(";")
 
 group = "io.masel"
-version = providers.environmentVariable("VERSION").getOrElse("3.1.0")
+version = providers.environmentVariable("VERSION").getOrElse("3.2.0")
 
 labyMod {
     defaultPackageName = "io.masel.nbtviewer"
