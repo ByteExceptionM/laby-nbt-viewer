4c178d5b4f4a1429c5dc7089bacaed85146034f2
Nycz-lab <nycz@nycz.club>
2026-06-23 12:41:12 +0200

    chore: bump deps to match addon template

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,11 @@
 rootProject.name = "nbt-viewer"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.6.2"
+    val labyGradlePluginVersion = "0.8.1"
 
     buildscript {
         repositories {
-            maven("https://dist.labymod.net/api/v1/maven/release/")
+            maven("https://maven.laby.net/api/v1/maven/release/")
             maven("https://maven.neoforged.net/releases/")
             maven("https://maven.fabricmc.net/")
             gradlePluginPortal()
